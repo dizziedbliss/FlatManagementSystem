6c9f3494cdd7403ad43ab15a0601d9f05fbd4249
Max!! <avengeraditya183@gmail.com>
2025-01-23 23:00:54 +0530

    Auto stash before merge of "master" and "origin/master"

---
+++ src/dsa.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "functions.h"
+#include "flats.h"
+
+const char dataset[50] = "data/data.csv";
+int current = 0; 
+flat residence[size];
+flat flats;
+
+int main(){
+    login();
+    read_file(); //reads the file data.csv and saves all the info to the RAM (residence array)
+    menufn(); //calls the main menu funtion
+
+    return 0;
+}
+
+//ver 3.0.0 stable
